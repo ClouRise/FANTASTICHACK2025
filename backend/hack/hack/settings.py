9d499bc2edd804452936e871a05baed442e7ff5b
Timur <kirichenkotimur@gmail.com>
2025-04-05 05:17:05 +0300

    *

--- backend/hack/hack/settings.py
+++ backend/hack/hack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0bdl$mlmo(p_qn8^x)v-1*t(pvouv7q47phigtj0$l76*i-oo=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
