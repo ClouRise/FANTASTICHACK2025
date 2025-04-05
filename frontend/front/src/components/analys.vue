983599bdd59d36d1e96a419cfae2908a2640f500
Boris <mr.clourise@gmail.com>
2025-04-05 13:45:32 +0300

    q

--- frontend/front/src/components/analys.vue
+++ frontend/front/src/components/analys.vue
@@ -1,56 +1,25 @@
 <template>
-    <div class="analys">
-        <div class="titles-analys">
-            <div class="row">
-                <titlerow style="box-shadow: #EFDE22 0px 0px 5px 3px" num="1"></titlerow>
-            </div>
-            <div class="row">
-                <titlerow style="box-shadow: #7A7A81 0px 0px 5px 3px" num="2"></titlerow>
-            </div>
-            <div class="row">
-                <titlerow style="box-shadow: #FF77004D 0px 0px 5px 3px" num="3"></titlerow>
-            </div>
-            <div class="row">
-                <titlerow num="4"></titlerow>
-            </div>
-            <div class="row">
-                <titlerow num="5"></titlerow>
-            </div>
-            <div class="row">
-                <titlerow num="6"></titlerow>
-            </div>
-        </div>
-        <div class="scroll-analys">
-            <div class="row">
-                <div style="height: 40px; width: 40px; ">
-                    <player style="margin-inline: 7px;" :size="40" :hcard="15" :wcard="25" :fontsize="12" :topminus="-10" fillColor="#ff0000" txt="1"></player>
-                </div>
-            </div>
-            <div class="row">
-                <div style="height: 40px; width: 40px; ">
-                    <player style="margin-inline: 7px;" :size="40" :hcard="15" :wcard="25" :fontsize="12" :topminus="-10" fillColor="#ff0000" txt="1"></player>
-                </div>
-            </div>
-            <div class="row">
-                <div style="height: 40px; width: 40px; ">
-                    <player style="margin-inline: 7px;" :size="40" :hcard="15" :wcard="25" :fontsize="12" :topminus="-10" fillColor="#ff0000" txt="1"></player>
-                </div>
-            </div>
-            <div class="row">
-                <div style="height: 40px; width: 40px; ">
-                    <player style="margin-inline: 7px;" :size="40" :hcard="15" :wcard="25" :fontsize="12" :topminus="-10" fillColor="#ff0000" txt="1"></player>
-                </div>
-            </div>
-            <div class="row">
-                <div style="height: 40px; width: 40px; ">
-                    <player style="margin-inline: 7px;" :size="40" :hcard="15" :wcard="25" :fontsize="12" :topminus="-10" fillColor="#ff0000" txt="1"></player>
-                </div>
-            </div>
-            <div class="row">
-                <div style="height: 40px; width: 40px; ">
-                    <player style="margin-inline: 7px;" :size="40" :hcard="15" :wcard="25" :fontsize="12" :topminus="-10" fillColor="#ff0000" txt="1"></player>
-                </div>
-            </div>
+    <div style="display: flex; flex-direction: row; justify-content: start; width: 100%; height: 100%;">
+        <article>
+            <div style="height: 30px;" class="cell-table"></div>
+            <div class="cell-table"><titlerow style="box-shadow: #EFDE22 0px 0px 5px 3px" num="1"></titlerow></div>
+            <div class="cell-table"><titlerow style="box-shadow: #7A7A81 0px 0px 5px 3px" num="2"></titlerow></div>
+            <div class="cell-table"><titlerow style="box-shadow: #FF77004D 0px 0px 5px 3px" num="3"></titlerow></div>
+            <div class="cell-table"><titlerow num="4"></titlerow></div>
+            <div class="cell-table"><titlerow num="5"></titlerow></div>
+            <div style="border: none; padding-bottom: 0;" class="cell-table"><titlerow num="6"е5></titlerow></div>
+        </article>
+        <div style="overflow-x: scroll; display: flex; flex-direction: row;">
+            <section>
+                <div style="height: 30px;" class="cell-table"><h3>№1</h3></div>
+                <div style="padding-block: 15px; width: 70px; height: 65px;" class="cell-table"><player :size="50" :wcard="30" :hcard="18" :fontsize="15" :topminus="-10" fillColor="#ff0000" txt="1"></player></div>
+                <div style="padding-block: 15px; width: 70px; height: 65px;" class="cell-table"><player :size="50" :wcard="30" :hcard="18" :fontsize="15" :topminus="-10" fillColor="#ff0000" txt="1"></player></div>
+                <div style="padding-block: 15px; width: 70px; height: 65px;" class="cell-table"><player :size="50" :wcard="30" :hcard="18" :fontsize="15" :topminus="-10" fillColor="#ff0000" txt="1"></player></div>
+                <div style="padding-block: 15px; width: 70px; height: 65px;" class="cell-table"><player :size="50" :wcard="30" :hcard="18" :fontsize="15" :topminus="-10" fillColor="#ff0000" txt="1"></player></div>
+                <div style="padding-block: 15px; width: 70px; height: 65px;" class="cell-table"><player :size="50" :wcard="30" :hcard="18" :fontsize="15" :topminus="-10" fillColor="#ff0000" txt="1"></player></div>
+                <div style="padding-block: 15px; width: 70px; height: 65px; border: none;" class="cell-table"><player :size="50" :wcard="30" :hcard="18" :fontsize="15" :topminus="-10" fillColor="#ff0000" txt="1"></player></div>
+            </section>
+
         </div>
     </div>
 </template>
@@ -67,24 +36,24 @@ export default {
 </script>
 
 <style scoped>
-.analys {
-    width: 610px;
-    display: flex;
-    flex-direction: row;
+h3{
+    font-family: ubuntu-medium;
+    color: white;
 }
-
-.row {
-    width: max-content;
+article{
+    display: flex;
+    flex-direction: column;
+    margin-left: 15px;
+}
+.cell-table{
+    padding-block: 12px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     border-bottom: 1pt solid rgb(46, 46, 46);
-    display: flex;
-    flex-direction: row;
-    justify-content: start;
-    padding: 5px;
-    padding-inline: 15px;
 }
-
-.scroll-analys {
-    width: auto;
-    overflow-x: scroll;
+section{
+    display: flex;
+    flex-direction: column;
 }
 </style>
