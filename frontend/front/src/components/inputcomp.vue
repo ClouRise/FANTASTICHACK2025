bf01377896edb149fd05ccd66a1fea5c88722bb9
artasvest <uragar2@gmail.com>
2025-04-06 01:55:01 +0300

    modalwindowbeta

---
+++ frontend/front/src/components/inputcomp.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input type="text">
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+input{
+    height: 30px;
+    width: 60px;
+    margin: 10px;
+    background-color: #2D2D2D;
+    border: none;
+    text-align: center;
+    border-radius: 5px;
+    font-size: 16px;
+    color: white;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    padding-bottom: 2px;
+    padding-inline: 5px;
+}
+
+</style>
--- frontend/front/src/components/modalwindow.vue
+++ frontend/front/src/components/modalwindow.vue
@@ -1,59 +1,133 @@
 <template>
    <div class="card">
-            <div class="header">
+
+            <div class="row">
+                <div class="header">
+                    <h2> Игрок </h2>
+                </div>
+
+
+                <div class="main">
+                    <player fillColor="#ff0000" txt="1" ></player>
+                    <player fillColor="#5C7CFA" txt="2" ></player>
+                    <player fillColor="#FCC419" txt="3" ></player>
+                    <player fillColor="#94D82D" txt="4" ></player>
+                    <player fillColor="#CC5DE8" txt="5" ></player>
+                    <player fillColor="#ffffff" txt="6" ></player>
+                </div>
+
             </div>
-            <div class="main">
+
+            <div class="row">
+                <div class="header">
+                    <h2> Время реакции на старте, c </h2>
+                </div>
+
+
+                <div class="main">
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                </div>
+
             </div>
-            <div class="header">
+
+            <div class="row">
+                <div class="header">
+                    <h2> Ускорение, <br> м/c </h2>
+                </div>
+
+
+                <div class="main">
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                </div>
+
             </div>
-            <div class="main">
+
+            <div class="row">
+                <div class="header">
+                    <h2> Макс. скорость, <br> м/c </h2>
+                </div>
+
+
+                <div class="main">
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                </div>
+
             </div>
-            <div class="header">
-            </div>
-            <div class="main">
-            </div>
-            <div class="header">
-            </div>
-            <div class="main">
-            </div>
-            <div class="header">
-            </div>
-            <div class="main">
-            </div>
+
+            <div class="row">
+                <div class="header">
+                    <h2> Коэффициент потери скорости </h2>
+                </div>
+
+
+                <div class="main">
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                    <inputcomp></inputcomp>
+                </div>
+
+            </div>  
+
+        
         </div>
 </template>
 
 <script>
+import player from './player.vue';
+import inputcomp from './inputcomp.vue';
     export default {
-        
+        components:{
+            player,
+            inputcomp
+        }
     }
 </script>
 
 <style scoped>
 .card {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     padding: 20px;
 }
 
 .header {
     background-color: #2D2D2D;
-    height: 40px;
+    height: 70px;
     display: flex;
     align-items: center;
-    justify-content: start;
-    padding-inline: 35px;
+    justify-content: center;
+    max-width: 120px;
+    min-width: 90px;
+    padding-inline: 5px;
 }
 
 .header h2 {
     font-family: ubuntu-bold;
     color: white;
-    font-size: 17px;
+    font-size: 15px;
+    text-align: center;
 }
 
 .main {
     background-color: #1D1D1D;
-    border-radius: 0 0 15px 15px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -61,5 +135,10 @@
     padding-top: 10px;
     padding-bottom: 15px;
     box-sizing: border-box;
+    flex-direction: column;
+}
+.row{
+    display: flex;
+    flex-direction:column;
 }
 </style>
